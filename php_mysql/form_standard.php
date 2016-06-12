96a52c50061e5062f3b7e935aca287b50a67c03e
Morasthite <Morasthite@gmail.com>
2016-06-12 13:53:21 -0700

    working on routing

---
+++ php_mysql/form_standard.php
@@ -0,0 +1,7 @@
+<form method="post" action="index_insert.php">
+    Title: <input name="title" id="title" type="text"><br>
+    Details: <input name="details" id="details" type="text"><br>
+    Timestamp: <input name="timestamp" id="timestamp" type="number"><br>
+    Arbitray Field: <input name="arbitrary" id="arbitrary" type="number"><br>
+    <button type="submit">Submit</button>
+</form>
