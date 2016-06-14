186e4d1dd3c3e01d65c6ef55b7297228a6cc9e0d
Morasthite <Morasthite@gmail.com>
2016-06-14 05:31:35 +0000

    facebook auth working, db insert working

--- php_oauth_facebook/login.php
+++ php_oauth_facebook/login.php
@@ -2,7 +2,7 @@
 session_start();
 //start your session
 //make sure to include your facebook credentials!
-require_once('../credentials.php');
+require_once('facebook_info.php');
 //then you'll need to include the facebook sdk
 require_once('libraries/facebook-php-sdk-v4-5.0.0/src/Facebook/autoload.php');
 //create a new facebook object
@@ -16,7 +16,7 @@ $fb = new Facebook\Facebook([
 
 $helper = $fb->getRedirectLoginHelper();
 //specify the permissions this app will need, putting them into an array
-$permissions = ['email'];
+$permissions = ['email','user_posts'];
 //generate the login url
 $loginUrl = $helper->getLoginUrl(SERVER_LANDING, $permissions);
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
