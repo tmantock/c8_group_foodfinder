c84d757acc0594fa380c3e2e904c5f1c5f0cf0d5
Eugene Kwon <ekwon86@gmail.com>
2016-06-16 18:04:51 -0700

    added dropdown menu and lets go button

--- landing.php
+++ landing.php
@@ -28,13 +28,13 @@ $loginUrl = $helper->getLoginUrl(SERVER_LANDING, $permissions);
     <h3 class="landing-heading">Food Search That's Truly Simple</h3>
     <h4 class="landing-heading">Find The Best Local Cuisine - Effortlessly</h4>
 
-    <div class="container landing-container">
+    <div class="landing-container">
          <?php $url = htmlspecialchars($loginUrl);
 if(empty($_SESSION["name"])){
 
       echo "<button type='button' class='btn btn-lg' id='login-button' > <i 
 class='fa fa-facebook-official fa-lg facebook-icon' aria-hidden='true'></i>
-<a  href='". $url ."'  class='login-link'>   Log in with Facebook </a></button>";
+<a  href='". $url ."'  class='login-link'> Log in with Facebook </a></button>";
 }//if name is empty
 else {
 echo "<div id='welcome_user'>Welcome ".$_SESSION["name"]."</div>";
@@ -66,7 +66,39 @@ echo "<div id='welcome_user'>Welcome ".$_SESSION["name"]."</div>";
         </div>
     </div>
 
-    <!-- Random Selection Button -->
+<section class="dropdown-container">
+    <div class="dropdown">
+        <select name="one" class="dropdown-select">
+            <option value="">What Type?</option>
+            <option value="1">American</option>
+            <option value="2">Japanese</option>
+            <option value="3">Chinese</option>
+        </select>
+    </div>
+    <div class="dropdown">
+        <select name="one" class="dropdown-select">
+            <option value="">How far?</option>
+            <option value="1">&lt; 1 mile</option>
+            <option value="2">3 miles</option>
+            <option value="3">&gt; 5 miles</option>
+        </select>
+    </div>
+    <div class="dropdown">
+        <select name="one" class="dropdown-select">
+            <option value="">How much?</option>
+            <option value="1">$</option>
+            <option value="2">$$</option>
+            <option value="3">$$$</option>
+            <option value="4">$$$$</option>
+        </select>
+    </div>
+    <div class="lets-go">
+        <div id="lets-go-text">Let's Go!</div>
+    </div>
+</section>
+<!--     Random Selection Button-->
     <div class="circle">
       <div id="random">Pick For Me!</div>
     </div>
+
+    <input type="text" placeholder="Please Enter ZIP Code" class="zip-input">
